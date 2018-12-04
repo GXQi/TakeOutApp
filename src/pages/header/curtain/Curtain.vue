9c83b51c966073ee293b2cd1c5220c837706727b
GXQi <15809237114@163.com>
2018-12-04 19:49:06 +0800

    write Curtain(完善stickyFooter)

--- src/pages/header/curtain/Curtain.vue
+++ src/pages/header/curtain/Curtain.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="curtain">
     <div class="curtain-wrapper clearfix">
-      <div class="curtain-header">
-        <div class="curtain-title">{{ seller.name }}</div>
-        <div class="curtain-star"></div>
-      </div>
-      <div class="curtain-discount" v-if="seller.supports">
-        <div class="curtain-discountTitle">优惠信息</div>
-        <div class="curtain-discountInfo">
-          <ul>
-            <li v-for="(item, index) in seller.supports" :key="index">
-              <span class="minus"></span>
-              <span class="text">{{ item.description }}</span>
-            </li>
-          </ul>
+      <div class="curtain-content">
+        <div class="curtain-header">
+          <div class="curtain-title">{{ seller.name }}</div>
+          <div class="curtain-star"></div>
         </div>
-      </div>
-      <div class="curtain-notice" v-if="seller.bulletin">
-        <div class="curtain-noticeTitle">商家公告</div>
-        <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+        <div class="curtain-discount" v-if="seller.supports">
+          <div class="curtain-discountTitle">优惠信息</div>
+          <div class="curtain-discountInfo">
+            <ul>
+              <li v-for="(item, index) in seller.supports" :key="index">
+                <span class="minus"></span>
+                <span class="text">{{ item.description }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="curtain-notice" v-if="seller.bulletin">
+          <div class="curtain-noticeTitle">商家公告</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+          <div class="curtain-noticeInfo">{{ seller.bulletin }}</div>
+        </div>
       </div>
     </div>
     <div class="curtain-close" @click="handleCurtainClick">
@@ -57,12 +66,16 @@ export default {
     background: rgba(7, 17, 27, .8)
     .curtain-wrapper
       min-height: 100%
-      padding: 2.56rem .72rem
+      .curtain-content
+        margin-top: 1.28rem
+        margin-left: .72rem
+        margin-right: .72rem
+        padding-bottom: 1.28rem
     .curtain-close
       position: relative
       width: .64rem
       height: .64rem
-      margin: auto
+      margin: -1.28rem auto 0 auto
       margin-top: -1.28rem
       font-size: .64rem
       clear: both
